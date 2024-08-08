3cf77b21600c5a54ca3dcab211f8d57b4fb10525
samargo <samargo0317@gmail.com>
2024-08-07 19:28:05 -0500

    feat (collect_cases) : collectcases works fine and minimization algorithm to each case

--- homework1/minimitation.py
+++ homework1/minimitation.py
@@ -1,4 +1,4 @@
-N = 6
+'''N = 6
 E = ["a"]
 F = [1, 4]
 M = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5,0]]
@@ -11,6 +11,7 @@ c_ = 0
 flag = False
 
 equivalent = []
+'''
 
 def search(n, n_1):
     T = []
@@ -21,24 +22,43 @@ def search(n, n_1):
             T.append(row.copy())
     return T
 
-for i in range(0, N):
-    minimitation.append([])
-    for j in range(0, N):
-        if i == j:
-            minimitation[i].append(i)
-        else:
-            minimitation[i].append('_')
+def search_equivalent(w, w_1):
+    global c 
+    global c_ 
+    global N
+    global M
+    global equivalent
 
-for i in range(0, N - 1):
-    for j in range(i + 1, N):
-        print(f"[{i}, {i}] [{j}, {j}]")
-        if minimitation[i][i] in F and minimitation[j][j] not in F or minimitation[i][i] not in F and minimitation[j][j] in F:
-            minimitation[j][i] = 'x'
+    if N - c != 0:
+        for i in range(w, w_1):
+            print(f"({i}, {c_})")
+            if minimitation[i][c_] == '_':
+                x = min(i, c_)
+                y =  max(i, c_)
+                equivalent.append(x)
+                equivalent.append(y)
+        c = c + 1
+        c_ = c_ + 1
+        search_equivalent(c, N)
+    return equivalent
 
-#c = 1
-#c_ = 0
-#step_3(c, N - c)
-def step_3(v, v_1):   
+def first_step(n):
+    for i in range(0, n):
+        minimitation.append([])
+        for j in range(0, n):
+            if i == j:
+                minimitation[i].append(i)
+            else:
+                minimitation[i].append('_')
+
+def second_step(n):
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            print(f"[{i}, {i}] [{j}, {j}]")
+            if minimitation[i][i] in F and minimitation[j][j] not in F or minimitation[i][i] not in F and minimitation[j][j] in F:
+                minimitation[j][i] = 'x'
+
+def third_step(v, v_1):   
     
     flag = False
 
@@ -86,44 +106,67 @@ def step_3(v, v_1):
         c = c + 1
         c_ = c_ + 1
 
-        step_3(c, N)
+        third_step(c, N)
 
         if flag:
             c = 1
             c_ = 0
-            step_3(c, N)
+            third_step(c, N)
             
-
-
-
-
-
-
-def search_equivalent(w, w_1):
-    global c 
-    global c_ 
-    global N
-    global M
-    global equivalent
-
-    if N - c != 0:
-        for i in range(w, w_1):
-            print(f"({i}, {c_})")
-            if minimitation[i][c_] == '_':
-                pair = (min(i, c_), max(i, c_))
-                equivalent.append(pair)
-        c = c + 1
-        c_ = c_ + 1
-        search_equivalent(c, N)
-    return equivalent
-
-step_3(c, N - c)
+'''
+first_step(N)
+second_step(N)
+third_step(c, N - c)
 
 for row in minimitation:
     print(row)
 
 c = 1
 c_ = 0
-
 print(search_equivalent(c, N - c))
+'''
 
+def collect_cases():
+    cases = []
+    c = int(input())
+
+    for i in range(0, c):
+        M = []
+        N = int(input())
+        E = [x for x in input().split(' ')]
+        F = [int(x) for x in input().split(' ')]
+
+        for j in range(0, N):
+            m = [int(x) for x in input().split(' ')]
+            M.append(m)
+
+        cases.append({
+            "N" : N,
+            "E" : E,
+            "F" : F,
+            "M" : M
+        })
+    return cases
+
+cases = collect_cases()
+
+for case in cases:
+    N = case["N"]
+    E = case["E"]
+    F = case["F"]
+    M = case["M"]
+    minimitation = []
+    c = 1
+    c_ = 0
+    flag = False
+    equivalent = []
+
+    first_step(N)
+    second_step(N)
+    third_step(c, N - c)
+
+    c = 1
+    c_ = 0
+
+    result = search_equivalent(c, N - c)
+    print(' '.join(str(x) for x in result))
