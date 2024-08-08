1ddf58e5336db76b05348b49d519aed8d8ad15a8
samargo <samargo0317@gmail.com>
2024-08-07 19:50:50 -0500

    feat (collect_data) : collect_data works fine. The program takes a txt file as an input and return the respective pair of equivalent states

--- homework1/minimitation.py
+++ homework1/minimitation.py
@@ -1,18 +1,3 @@
-'''N = 6
-E = ["a"]
-F = [1, 4]
-M = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5,0]]
-
-minimitation = []
-
-c = 1
-c_ = 0
-
-flag = False
-
-equivalent = []
-'''
-
 def search(n, n_1):
     T = []
     for row in M:
@@ -31,7 +16,6 @@ def search_equivalent(w, w_1):
 
     if N - c != 0:
         for i in range(w, w_1):
-            print(f"({i}, {c_})")
             if minimitation[i][c_] == '_':
                 x = min(i, c_)
                 y =  max(i, c_)
@@ -54,7 +38,6 @@ def first_step(n):
 def second_step(n):
     for i in range(0, n - 1):
         for j in range(i + 1, n):
-            print(f"[{i}, {i}] [{j}, {j}]")
             if minimitation[i][i] in F and minimitation[j][j] not in F or minimitation[i][i] not in F and minimitation[j][j] in F:
                 minimitation[j][i] = 'x'
 
@@ -68,39 +51,17 @@ def third_step(v, v_1):
     global M
 
     if N - c != 0:
-        print("-------")
-        print(v)
-        print(v_1)
-        print("-------")
         for i in range(v, v_1):
-            print(f"({i}, {c_})")
             if minimitation[i][c_] == '_':
-                print(f"{c_}, {i}")
                 T = search(c_, i)
-                print("Hola")
-                print(T)
-                print(T[0])
-                print(T[1])
                 T[0].pop(0)
                 T[1].pop(0)
-                print(T)
-                print("####")
-                print(M)
-                print("####")
                 while flag == False and len(T[0]) > 0:
-                    print(T)
                     f = T[0].pop(0)
                     f_1 = T[1].pop(0)
-                    print(f)
-                    print(f_1)
                     row = max(f, f_1)
                     col = min(f, f_1)
-                    print("-------")
-                    print(row)
-                    print(col)
-                    print("-------")
                     if minimitation[row][col] == 'x':
-                        print("XXXXXXXXXXXXXXXXXXXXXXXXXX")
                         minimitation[i][c_] = 'x'
                         flag = True
         c = c + 1
@@ -113,19 +74,6 @@ def third_step(v, v_1):
             c_ = 0
             third_step(c, N)
             
-'''
-first_step(N)
-second_step(N)
-third_step(c, N - c)
-
-for row in minimitation:
-    print(row)
-
-c = 1
-c_ = 0
-print(search_equivalent(c, N - c))
-'''
-
 def collect_cases():
     cases = []
     c = int(input())
